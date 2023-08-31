0ca9e50ccdc02d6527d87bab7eb5824029750301
Viganesh1 <143755987+Viganesh1@users.noreply.github.com>
2023-08-31 17:09:20 -0400

    Add files via upload

---
+++ CMakeProject1.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Viganesh Ramachandran" << endl;
+	return 0;
+}
